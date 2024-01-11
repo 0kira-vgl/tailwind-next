cfa79a13b1e7bae23d7c046cf88424691e482ac4
Matheus Henrique <matheusgtiburcio@gmail.com>
2024-01-11 11:47:50 -0300

    add tab + Framer Motion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import { SettiingTabs } from "@/components/SettiingTabs";
+
 export default function Home() {
   return (
-    <div>
-      <h1>ola</h1>
-    </div>
+    <>
+      <h1 className="text-3xl font-medium text-zinc-900">Settings</h1>
+
+      <SettiingTabs />
+    </>
   );
 }
