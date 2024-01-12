fa319d8503081e014edd791df3f22585cba8fc19
Matheus Henrique <matheusgtiburcio@gmail.com>
2024-01-12 19:07:18 -0300

    add animations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -30,6 +30,22 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+
+    keyframes: {
+      slideDownAndFade: {
+        from: { opacity: 0, trasforme: "translateY(-2px)" },
+        to: { opacity: 1, trasforme: "translateY(0)" },
+      },
+      // slideUpAndFade: {
+      //   from: { opacity: 1 },
+      //   to: { opacity: 0 },
+      // },
+    },
+
+    animation: {
+      slideDownAndFade: "slideDownAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
+      // slideUpAndFade: "slideUpAndFade 1s linear",
+    },
   },
   plugins: [],
 };
