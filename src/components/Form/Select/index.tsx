000ebbb274f9bcdb471f678ef2986c498928dbd3
Matheus Henrique <matheusgtiburcio@gmail.com>
2024-01-13 10:23:04 -0300

    add focus effects

--- src/components/Form/Select/index.tsx
+++ src/components/Form/Select/index.tsx
@@ -11,7 +11,7 @@ export interface SelectProps extends SelectPrimitive.SelectProps {
 export function Select({ children, placeholder, ...props }: SelectProps) {
   return (
     <SelectPrimitive.Root {...props}>
-      <SelectPrimitive.Trigger className="flex h-11 w-full items-center justify-between gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm data-[placeholder]:text-zinc-600 data-[placeholder]:select-none">
+      <SelectPrimitive.Trigger className="flex h-11 w-full items-center justify-between gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm data-[placeholder]:text-zinc-600 data-[placeholder]:select-none outline-none focus:border-violet-300 focus:ring-2 focus:ring-violet-100">
         <SelectPrimitive.Value
           placeholder={placeholder}
           className="text-black"
